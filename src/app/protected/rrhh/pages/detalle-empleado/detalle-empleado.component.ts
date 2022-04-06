9d1c6d0e56d0db79a8785a9a80c447461da3ccb8
Marcelo Jaimes <marceloeziojaimes@gmail.com>
2022-04-06 19:08:09 -0400

    more changes in the desing

--- src/app/protected/rrhh/pages/detalle-empleado/detalle-empleado.component.ts
+++ src/app/protected/rrhh/pages/detalle-empleado/detalle-empleado.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RrhhService } from '../../services/rrhh.service';
 import { Empleado } from '../../../interfaces/Empleado';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-detalle-empleado',
@@ -10,6 +11,11 @@ import { Empleado } from '../../../interfaces/Empleado';
 export class DetalleEmpleadoComponent implements OnInit {
 
   regEmp !: Empleado;
+  itemsDatosPersonales!: MenuItem[];
+  /**
+   * Lista para los SplitButton
+  */
+
 
   constructor(
     private empleadoService: RrhhService
@@ -17,6 +23,30 @@ export class DetalleEmpleadoComponent implements OnInit {
 
   ngOnInit(): void {
     this.obtenerDetalleEmpleado();
+    this.cargarOpcionesDatosEmpleado();
+  }
+
+  /**
+   * Cargar opcione smenu boton editar datos Personales
+   */
+  cargarOpcionesDatosEmpleado(): void {
+
+    this.itemsDatosPersonales = [
+      {
+        label: 'Update', icon: 'pi pi-refresh', command: () => {
+
+        }
+      },
+      {
+        label: 'Delete', icon: 'pi pi-times', command: () => {
+
+        }
+      },
+      { label: 'Angular.io', icon: 'pi pi-info', url: 'http://angular.io' },
+
+      { label: 'Setup', icon: 'pi pi-cog', routerLink: ['/setup'] }
+    ];
+
   }
 
   /**
