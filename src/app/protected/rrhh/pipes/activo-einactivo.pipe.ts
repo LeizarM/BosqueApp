07a2bfd4a082f93fc021d799b68d0c7e20255ca1
Marcelo Jaimes <marceloeziojaimes@gmail.com>
2022-05-11 18:10:52 -0400

    pipe fix

--- src/app/protected/rrhh/pipes/activo-einactivo.pipe.ts
+++ src/app/protected/rrhh/pipes/activo-einactivo.pipe.ts
@@ -7,7 +7,9 @@ import { lstEstadoActivoInactivo } from '../../interfaces/Tipos';
 export class ActivoEInactivoPipe implements PipeTransform {
 
   transform( valor : number = 1 ): string {
-    if(!valor) return "-Estado Desconocido-";
+    /* if( valor != 0 && valor != 1   ) {
+      return "-Estado Desconocido-";
+    } */
     return lstEstadoActivoInactivo().filter(exp => Number(exp.codTipos) === valor)[0].nombre;
   }
 
